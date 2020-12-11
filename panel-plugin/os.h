036e979784489a6dbcf59d8acf78320a4e5dd014
Jan Ziak <0xe2.0x9a.0x9b@gmail.com>
2020-12-12 00:34:34 +0100

    Highlighting of suboptimal thread placement on SMT CPUs
    The base idea is to detect when multiple threads have been running
    on the same CPU core and then try to find (to compute) whether a better
    placement of threads on the CPU cores might have been possible.
    
    If a more optimal thread placement is found, cpugraph will then
    try to compute how much it impacts the overall performance of the
    machine and how much it momentarily impacts the applications subjected to
    suboptimal thread placement.
    
    The approximate slowdown if two threads are executed on the same
    physical core instead of being executed on separate cores is about 25%.
    This number has been determined by measuring the slowdown
    of running two instances of "stress-ng --cpu=1" on a Ryzen 3700X CPU.
    Note that the slowdown might be different for applications other
    than stress-ng and on CPUs other than Ryzen 3700X. However, 25% seems like
    a reasonable generic value for use in the cpugraph's algorithm determining
    the approximate performance impact of suboptimal SMT scheduling decisions.
    
    A description of what is being shown to the user:
    
     - In the CPU bars drawing area:
        - Bars corresponding to threads experiencing SMT scheduling issues
          are (optionaly) painted using a different color
     - In cpugraphs' properties dialog:
        - Whether the CPU supports SMT
        - Number of SMT scheduling incidents detected by cpugraph
        - An estimate of how suboptimal placement of threads might have
          impacted the overall and the momentary performance of the machine
    
    Suboptimal SMT scheduling cases are quite rare (at least in Linux):
    
     - They are impossible to happen if the CPU is under full load
     - They are rare if the CPU is running one single-threaded task
     - They tend to occur especially when the CPU is running
       about $(nproc)/2 threads
    
    This feature is currently supported only on Linux and isn't supported
    on other Unix-style operating systems.
    
    Closes: https://gitlab.xfce.org/panel-plugins/xfce4-cpugraph-plugin/-/issues/11

--- panel-plugin/os.h
+++ panel-plugin/os.h
@@ -31,9 +31,43 @@ typedef struct
     gfloat load; /* Range: from 0.0 to 1.0 */
     guint64 previous_used;
     guint64 previous_total;
+    gboolean smt_highlight;
 } CpuData;
+
+typedef struct
+{
+    guint num_smt_incidents;
+    struct {
+        /* Estimated performance loss (%): 100 * (optimal - actual) / actual */
+        struct {
+            gdouble actual, optimal;
+        } during_smt_incidents;
+        struct {
+            gdouble actual, optimal;
+        } total;
+    } num_instructions_executed;
+} CpuStats;
+
+/* All pointers in this data structure are internal to a single memory allocation
+ * and thus the whole data structure can be deallocated using a single call to g_free().
+ * Consequenly, pointers exported/read from this data structure are invalid after the deallocation. */
+typedef struct
+{
+    guint num_all_logical_cpus;
+    guint num_online_logical_cpus;
+    guint num_all_cores;      /* Range: <1, num_all_logical_cpus> */
+    guint num_online_cores;   /* Range: <1, num_online_logical_cpus> */
+    gint *logical_cpu_2_core; /* Maps a logical CPU to its core, or to -1 if offline */
+    struct CpuCore {
+        guint num_logical_cpus; /* Number of logical CPUs in this core. Might be zero. */
+        guint *logical_cpus;    /* logical_cpus[i] range: <0, num_all_logical_cpus> */
+    } *cores;
+    gboolean smt;       /* Simultaneous multi-threading (hyper-threading) */
+    gdouble smt_ratio;  /* Equals to (num_online_logical_cpus / num_online_cores), >= 1.0 */
+} Topology;
 
 guint detect_cpu_number (void);
 gboolean read_cpu_data (CpuData *data, guint nb_cpu);
+Topology* read_topology (void);
 
 #endif /* !_XFCE_OS_H */
