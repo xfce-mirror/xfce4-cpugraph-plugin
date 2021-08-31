1525d6815c483c7f1d60267ce5b64604e55196b7
Jan Ziak <0xe2.0x9a.0x9b@xfce.org>
2021-08-31 20:52:41 +0200

    Rename main plugin implementation file from C to C++
    The main purpose of this patch is to replace cpu.c with cpu.cc.
    
    This step requires some #include directives of libxfce4util to be enclosed
    in extern "C" {} because some of the included Xfce4 C files are missing
    G_BEGIN_DECLS/G_END_DECLS macros. This issue has been fixed in
    libxfce4util's main GitLab repository, but (1) there hasn't been a release
    containing this fix and (2) it would delay cpugraph adoption in
    conservative Linux distributions which are upgrading to newer Xfce
    library releases less often.
    
    All C++ implementation files (.cc file extension) should include fixes.h
    before including any other header files.
    
    The new file plugin.c, comprising of a single line of code, is required
    because XFCE_PANEL_PLUGIN_REGISTER isn't fully compatible with C++.
    
    See also: https://gitlab.xfce.org/xfce/libxfce4util/-/merge_requests/19

---
+++ panel-plugin/plugin.c
@@ -0,0 +1,23 @@
+/*  plugin.c
+ *  Part of xfce4-cpugraph-plugin
+ *
+ *  Copyright (c) 2021 Jan Ziak <0xe2.0x9a.0x9b@xfce.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "cpu.h"
+
+XFCE_PANEL_PLUGIN_REGISTER (cpugraph_construct);
